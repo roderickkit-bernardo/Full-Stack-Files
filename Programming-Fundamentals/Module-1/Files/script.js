234311117ad519c771bdd31694fe591b8c0e03e0
Roderick Bernardo <roderickkit.bernardo@gmail.com>
2026-05-23 13:36:32 -0400

    Course updates

--- Programming-Fundamentals/Module-1/Files/script.js
+++ Programming-Fundamentals/Module-1/Files/script.js
@@ -1,2 +1,8 @@
 // This is visible in the browser’s developer tools.
 console.log("Hello World");
+
+(async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const json = await response.json();
+  console.log(json);
+})();
